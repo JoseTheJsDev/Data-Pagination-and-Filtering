7701a533286c48e501f1f142b0fa925e7617f06a
Jose Munoz <jmunozjr17@gmail.com>
2022-10-30 03:14:10 -0500

    Display Page

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
-
+console.log(data);
 
 
 /*
@@ -18,8 +18,30 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+function showPage (list, page)  {
+   const startIndex = (page * 9) - 9;
+   const endIndex = page * 9;
+   const studentList = document.querySelector(".student-list");
+   studentList.innerHTML = '';
 
-
+   for(let i = 0; i > list.length; i ++ ) {
+      if (i >= startIndex && i < endIndex) {
+          let studentItem = 
+         `<li class="student-item cf">
+          <div class="student-details">
+          <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+          <h3>${list[i].name.first} ${list[i].name.last}</h3>
+          <span class="email">${list[i].email}</span>
+        </div>
+        <div class="joined-details">
+          <span class="date">${list[i].registered.date}{</span>
+        </div>
+      </li>`;
+      studentList.insertAdjacentHTML("beforeend", studentItem);
+      }
+   }
+};
+showPage(data, 1);
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons

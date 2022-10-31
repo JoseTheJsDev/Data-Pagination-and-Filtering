a593d0c97fe3988a9daaefad9e228b470d072bd2
Jose Munoz <jmunozjr17@gmail.com>
2022-10-31 13:28:05 -0500

    updated changes

--- js/script.js
+++ js/script.js
@@ -4,18 +4,9 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 
 /*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-
 
 function showPage (list, page)  {
    const startIndex = page * 9 - 9;
@@ -44,7 +35,25 @@ showPage(data, 2);
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-
-
+function addPagination(list) {
+   // create a variable to calculate the number of pages needed
+   const totalPages = data.length || 9;
+ 
+   // select the element with a class of `link-list` and assign it to a variable
+    const linkList = document.querySelector('.link-list');
+   // set the innerHTML property of the variable you just created to an empty string
+    linkList.innerHTML = '';
+   // loop over the number of pages needed
+     // create the elements needed to display the pagination button
+     // insert the above elements
+ 
+   // give the first pagination button a class of "active"
+ 
+   // create an event listener on the `link-list` element
+     // if the click target is a button:
+       // remove the "active" class from the previous button
+       // add the active class to the clicked button
+       // call the showPage function passing the `list` parameter and page to display as arguments
+}
 
 // Call functions

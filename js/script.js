d8bd85225af590403564a34d7b80fcb34ac788e1
Jose Munoz <jmunozjr17@gmail.com>
2022-11-22 20:11:56 -0600

    addPagination Function

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ This function will create and insert/append the elements needed to display a "pa
 */
 
 function showPage (list, page)  {
+
    const startIndex = page * 9 - 9;
    const endIndex = page * 9;
    const studentList = document.querySelector(".student-list");
@@ -30,23 +31,35 @@ function showPage (list, page)  {
       }
    }
 };
-showPage(data, 2);
+showPage(data, 1);
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 function addPagination(list) {
    // create a variable to calculate the number of pages needed
-   const totalPages = data.length || 9;
+   const totalPages = data.length / 9;
+   const buttons = 0;
  
    // select the element with a class of `link-list` and assign it to a variable
     const linkList = document.querySelector('.link-list');
    // set the innerHTML property of the variable you just created to an empty string
     linkList.innerHTML = '';
    // loop over the number of pages needed
+      for(let i = 0; i < totalPages.length; i++){
+      };
      // create the elements needed to display the pagination button
+     const pageNumbers = document.createElement('li');
+     const pageButton = document.createElement('button');
+     buttons += 1;
+     pageButton.innerHTML = buttons;
+     linkList.appendChild(pageNumbers);
+     pageNumbers.appendChild(pageButton);
+     linkList.firstChild.firstChild.setAttribute('class', 'active');
+     
+
      // insert the above elements
- 
+      
    // give the first pagination button a class of "active"
  
    // create an event listener on the `link-list` element
@@ -54,6 +67,6 @@ function addPagination(list) {
        // remove the "active" class from the previous button
        // add the active class to the clicked button
        // call the showPage function passing the `list` parameter and page to display as arguments
-}
 
 // Call functions
+    }

31280975625ecab2b027c774fb5f76ca14199b85
Jose Munoz <jmunozjr17@gmail.com>
2022-12-06 20:38:29 -0600

    Student SearchBar
    Cant get search bar to display to the page.

--- js/script.js
+++ js/script.js
@@ -7,12 +7,15 @@ FSJS Project 2 - Data Pagination and Filtering
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-
+//Global Variables
+const studentList = document.querySelector(".student-list");
+const studentSearchBar = document.querySelector(".header");
+const studentData = data;
+//This function will create and insert/append the elements needed to display a "page" of nine students
 function showPage (list, page)  {
 
    const startIndex = page * 9 - 9;
    const endIndex = page * 9;
-   const studentList = document.querySelector(".student-list");
    studentList.innerHTML = '';
 
    for(let i = 0; i < list.length; i ++ ) {
@@ -31,57 +34,53 @@ function showPage (list, page)  {
       }
    }
 };
-showPage(data, 1);
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+
 function addPagination(list) {
    // create a variable to calculate the number of pages needed
-   const numOfPages = Math.ceil(list.length / 9);
+   const numOfPages = Math.ceil(list.length/ 9);
  
    // select the element with a class of `link-list` and assign it to a variable
-    const linkList = document.querySelector('.link-list');
+   const linkList = document.querySelector('.link-list');
    // set the innerHTML property of the variable you just created to an empty string
     linkList.innerHTML = '';
    // loop over the number of pages needed
-      for(let i = 0; i < numOfPages.length; i++){
+      for(let i = 1; i < numOfPages; i++){
         let button = `
         <li>
         <button type="button">${i}</button>
       </li>`;
         linkList.insertAdjacentHTML('beforeend', button);
+        let buttonClass = document.querySelector('button');
+        buttonClass.className = 'active';
+        linkList.addEventListener('click', (e) => {
+          if ( e.target.getAttribute('type') === 'button') {
+             document.querySelector('.active').className = '';
+             e.target.className = 'active';
+             let text = e.target.textContent;
+             showPage(data, text);
+          }
+         });
       }
-     // create the elements needed to display the pagination button
      
 }
 
-let buttonClass = document.querySelector('button');
-buttonClass.className = 'active';
-
-linkList.addEventListener('click', (e) => {
- if ( e.target.getAttribute('type') === 'button') {
-    document.querySelector('.active').className = '';
-    e.target.className = 'active';
-    let text = e.target.textContent;
-    showPage(data, text);
- }
-});
 
 // Call functions
 showPage(data, 1);
 addPagination(data);
 
-    
-     // insert the above elements
-      
-   // give the first pagination button a class of "active"
- 
-   // create an event listener on the `link-list` element
-     // if the click target is a button:
-       // remove the "active" class from the previous button
-       // add the active class to the clicked button
-       // call the showPage function passing the `list` parameter and page to display as arguments
+//Function that creates a search bar to filter through the students.
+function studentSearch () {
+  const searchBar = `<label for="search" class="student-search">
+  <span>Search by name</span>
+  <input id="search" placeholder="Search by name...">
+  <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+</label>`;
+  studentSearchBar.insertAdjacentHTML('beforeend', studentSearchBar);
+  studentSearchBar.addEventListner('keyUp', (e) => {
+    console.log(e);
 
-// Call functions
-    
+  })
+}
+
+

403135ab1b6445b4bf63988f81c1e5513c8c4c42
Jose Munoz <jmunozjr17@gmail.com>
2022-12-07 18:37:11 -0600

    Finished Project

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
 //Global Variables
 const studentList = document.querySelector('.student-list');
 const studentSearchBar = document.querySelector('.header');
@@ -19,6 +11,7 @@ function showPage (list, page)  {
    const endIndex = page * 9;
    studentList.innerHTML = '';
 
+   //For loop that loops through the array of students and creates an element for each student
    for(let i = 0; i < list.length; i ++ ) {
       if (i >= startIndex && i < endIndex) {
           let studentItem = `<li class="student-item cf">
@@ -35,17 +28,14 @@ function showPage (list, page)  {
       }
    }
 };
-
-function addPagination(list) {
-   // create a variable to calculate the number of pages needed
+    // This function creates and apppends the elements needed for the pagination buttons
+   function addPagination(list) {
+   // This variable calculates the number of pages needed
    const numOfPages = Math.ceil(list.length/ 9);
- 
-   // select the element with a class of `link-list` and assign it to a variable
    const linkList = document.querySelector('.link-list');
-   // set the innerHTML property of the variable you just created to an empty string
-    linkList.innerHTML = '';
-   // loop over the number of pages needed
-      for(let i = 1; i < numOfPages; i++){
+   linkList.innerHTML = '';
+   // For loop that loops over the number of pages needed
+      for(let i = 1; i <= numOfPages; i++){
         let button = `
         <li>
         <button type="button">${i}</button>
@@ -77,10 +67,22 @@ function studentSearch () {
   <input id="search" placeholder="Search by name...">
   <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
 </label>`;
-  studentSearchBar.insertAdjacentHTML('beforeend', studentSearchBar);
+  studentSearchBar.insertAdjacentHTML('beforeend', searchBar);
+  const searchingStudents = document.querySelector('#search');
   studentSearchBar.addEventListener('keyup', (e) => {
-    console.log(e);
-
+    let matches = []
+      for (const student of studentData) {
+         const fullName = `${student.name.first} ${student.name.last}`.toLowerCase()
+         if (fullName.includes(searchingStudents.value.toLowerCase() )) {
+            matches.push(student)
+         }
+      }
+      showPage(matches, 1);
+      addPagination(matches);
+      // if statement for showing "no results'
+      if (matches.length === 0) {
+         studentList.insertAdjacentHTML('beforeend', `<h1>No Results</h1>`)
+      }
   });
 };
 studentSearch(data);

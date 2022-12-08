2abb59491f4a07a8b0f4adf42c61502226ef7960
Jose Munoz <jmunozjr17@gmail.com>
2022-12-07 18:53:55 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const studentSearchBar = document.querySelector('.header');
 const studentData = data;
 
 //This function will create and insert/append the elements needed to display a "page" of nine students
-function showPage (list, page)  {
+  function showPage (list, page)  {
    const startIndex = page * 9 - 9;
    const endIndex = page * 9;
    studentList.innerHTML = '';
@@ -13,16 +13,19 @@ function showPage (list, page)  {
    //For loop that loops through the array of students and creates an element for each student
    for(let i = 0; i < list.length; i ++ ) {
       if (i >= startIndex && i < endIndex) {
-          let studentItem = `<li class="student-item cf">
-          <div class="student-details">
-          <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+
+        let studentItem = `
+        <li class="student-item cf">
+         <div class="student-details">
+          <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture" />
           <h3>${list[i].name.first} ${list[i].name.last}</h3>
           <span class="email">${list[i].email}</span>
-        </div>
-        <div class="joined-details">
+         </div>
+         <div class="joined-details">
           <span class="date">${list[i].registered.date}</span>
-        </div>
-      </li>`;
+         </div>
+        </li>
+      `;
       studentList.insertAdjacentHTML("beforeend", studentItem);
       }
    }

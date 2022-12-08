d0b1c1bd919dccf7d4238d3be0b294b0379dab6e
Jose Munoz <jmunozjr17@gmail.com>
2022-12-07 18:55:36 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,14 +16,14 @@ const studentData = data;
 
         let studentItem = `
         <li class="student-item cf">
-         <div class="student-details">
-          <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture" />
-          <h3>${list[i].name.first} ${list[i].name.last}</h3>
-          <span class="email">${list[i].email}</span>
-         </div>
-         <div class="joined-details">
-          <span class="date">${list[i].registered.date}</span>
-         </div>
+          <div class="student-details">
+            <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture" />
+            <h3>${list[i].name.first} ${list[i].name.last}</h3>
+            <span class="email">${list[i].email}</span>
+          </div>
+          <div class="joined-details">
+            <span class="date">${list[i].registered.date}</span>
+          </div>
         </li>
       `;
       studentList.insertAdjacentHTML("beforeend", studentItem);
@@ -38,9 +38,9 @@ const studentData = data;
       linkList.innerHTML = '';
 
    // For loop that loops over the number of pages needed
-        for(let i = 1; i <= numOfPages; i++){
-          let button = `
-          <li>
+      for(let i = 1; i <= numOfPages; i++){
+       let button = `
+        <li>
           <button type="button">${i}</button>
         </li>`;
           linkList.insertAdjacentHTML('beforeend', button);

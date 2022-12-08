dd286a5e7fdd377e61d66e4aa389b2c0ab856b4e
Jose Munoz <jmunozjr17@gmail.com>
2022-12-07 18:44:40 -0600

    Invoking Functions Moved To Bottom Of The Page

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ const studentData = data;
 
 //This function will create and insert/append the elements needed to display a "page" of nine students
 function showPage (list, page)  {
-
    const startIndex = page * 9 - 9;
    const endIndex = page * 9;
    studentList.innerHTML = '';
